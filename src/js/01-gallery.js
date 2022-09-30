dabd30428564cf701e3c17ff3f160863017095fb
Yurii PErekrestnyi <jodlei1618@gmail.com>
2022-09-30 23:26:17 +0300

    change src

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const items = galleryItems;
+const container = document.querySelector('.gallery');
+const markup = items.reduce(
+  (acc, items) =>
+    acc +
+    `<div class="gallery__item">
+  <a class="gallery__link" href=${items.original}>
+    <img
+      class="gallery__image"
+      src=${items.preview}
+      data-source=${items.original}
+      alt=${items.description}
+    />
+  </a>
+</div>`,
+  ''
+);
+container.insertAdjacentHTML('beforeend', markup);
 
-console.log(galleryItems);
+container.addEventListener('click', onItem);
+
+function onItem(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const options = {
+    onShow: () => window.addEventListener('keydown', HandleEsc),
+    onClose: () => window.removeEventListener('keydown', HandleEsc),
+  };
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    options
+  );
+  instance.show();
+  function HandleEsc(evt) {
+    if (evt.code === `Escape`) {
+      instance.close();
+    }
+  }
+}
